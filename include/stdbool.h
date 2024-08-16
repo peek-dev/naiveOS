f4a42f82a828d7dcbaa35b11bcd0e5688ca7644e
Paul D. Karhnak <pdevkarhnak@gmail.com>
2024-08-16 08:20:37 -0600

    Brought stdbool.h into compliance with C11 standard.

--- include/stdbool.h
+++ include/stdbool.h
@@ -1,9 +1,15 @@
 #ifndef __SKYLOS_STDBOOL_H__
 #define __SKYLOS_STDBOOL_H__
 
-typedef unsigned char bool;
+/*
+ * Define standard boolean type and macros according to C11 standard.
+ */
 
-#define true ((bool) 1)
-#define false ((bool) 0)
+#define bool _Bool
+
+#define true 1
+#define false 0
+
+#define __bool_true_false-are_defined 1
 
 #endif
