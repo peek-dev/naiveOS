e66f7766a8bfd43cbd8cbbea13cf43620797019f
Paul D. Karhnak <pdevkarhnak@gmail.com>
2024-08-16 08:15:41 -0600

    Implemented iso646.h more "directly" from the C11 standard instead of from GCC.

--- include/iso646.h
+++ include/iso646.h
@@ -1,45 +1,52 @@
-/* Copyright (C) 1997-2022 Free Software Foundation, Inc.
-
-This file is part of GCC.
-
-GCC is free software; you can redistribute it and/or modify
-it under the terms of the GNU General Public License as published by
-the Free Software Foundation; either version 3, or (at your option)
-any later version.
-
-GCC is distributed in the hope that it will be useful,
-but WITHOUT ANY WARRANTY; without even the implied warranty of
-MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-GNU General Public License for more details.
-
-Under Section 7 of GPL version 3, you are granted additional
-permissions described in the GCC Runtime Library Exception, version
-3.1, as published by the Free Software Foundation.
-
-You should have received a copy of the GNU General Public License and
-a copy of the GCC Runtime Library Exception along with this program;
-see the files COPYING3 and COPYING.RUNTIME respectively.  If not, see
-<http://www.gnu.org/licenses/>.  */
+#ifndef __SKYLOS_ISO646_H__
+#define __SKYLOS_ISO646_H__
 
 /*
- * ISO C Standard:  7.9  Alternative spellings  <iso646.h>
+ * Define alternatively spellings according to section 7.9 of the C11 standard.
  */
 
-#ifndef _ISO646_H
-#define _ISO646_H
-
-#ifndef __cplusplus
-#define and	&&
-#define and_eq	&=
-#define bitand	&
-#define bitor	|
-#define compl	~
-#define not	!
-#define not_eq	!=
-#define or	||
-#define or_eq	|=
-#define xor	^
-#define xor_eq	^=
+#ifndef and
+#define and &&
 #endif
+
+#ifndef and_eq
+#define and_eq &=
+#endif 
+
+#ifndef bitand
+#define bitand &
+#endif 
+
+#ifndef bitor
+#define bitor |
+#endif 
+
+#ifndef compl
+#define compl ~
+#endif 
+
+#ifndef not
+#define not !
+#endif 
+
+#ifndef not_eq
+#define not_eq !=
+#endif 
+
+#ifndef or
+#define or ||
+#endif 
+
+#ifndef or_eq
+#define or_eq |=
+#endif 
+
+#ifndef xor
+#define xor ^
+#endif 
+
+#ifndef xor_eq
+#define xor_eq ^=
+#endif 
 
 #endif
